ac3003f0a36e410d614d53cadeb0c66ef382b0ce
Steven Zhang <1581647952@qq.com>
2023-09-27 08:21:09 +0800

    feat(MemberItem): Add pulse animation

--- src/components/MemberItem.vue
+++ src/components/MemberItem.vue
@@ -25,7 +25,7 @@ export default {
 </script>
 
 <template>
-  <div class="flex items-center gap-x-6">
+  <div class="flex items-center gap-x-6" v-show="memberItem?.login? true: false">
     <img class="h-16 w-16 rounded-full" :src="memberItem?.avatar_url" alt="" />
     <div>
       <h3
@@ -42,5 +42,16 @@ export default {
         }}</span>
       </h3>
     </div>
+  </div>
+  <div class="animate-pulse flex space-x-4" v-show="!memberItem?.login? true: false">
+      <div class="rounded-full bg-slate-200 h-16 w-16"></div>
+      <div class="flex-1 space-y-6 py-1">
+        <div class="h-2 bg-slate-200 rounded"></div>
+        <div class="space-y-3">
+          <div class="grid grid-cols-3 gap-4">
+            <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
--- src/member-item.d.ts
+++ src/member-item.d.ts
@@ -1,4 +1,5 @@
 export interface MemberItem extends Object {
+  readonly message: string | null;
   readonly login: string;
   readonly html_url: string;
   readonly avatar_url: string;
