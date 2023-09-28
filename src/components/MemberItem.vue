c7260e462cd768cc978228bad4121cdf30316111
Steven Zhang <1581647952@qq.com>
2023-09-28 09:03:29 +0800

    docs(members): Delete member.json

--- src/components/MemberItem.vue
+++ src/components/MemberItem.vue
@@ -25,7 +25,10 @@ export default {
 </script>
 
 <template>
-  <div class="flex items-center gap-x-6" v-show="memberItem?.login? true: false">
+  <div
+    class="flex items-center gap-x-6"
+    v-show="memberItem?.login ? true : false"
+  >
     <img class="h-16 w-16 rounded-full" :src="memberItem?.avatar_url" alt="" />
     <div>
       <h3
@@ -43,15 +46,18 @@ export default {
       </h3>
     </div>
   </div>
-  <div class="animate-pulse flex space-x-4" v-show="!memberItem?.login? true: false">
-      <div class="rounded-full bg-slate-200 h-16 w-16"></div>
-      <div class="flex-1 space-y-6 py-1">
-        <div class="h-2 bg-slate-200 rounded"></div>
-        <div class="space-y-3">
-          <div class="grid grid-cols-3 gap-4">
-            <div class="h-2 bg-slate-200 rounded col-span-1"></div>
-          </div>
+  <div
+    class="animate-pulse flex space-x-4"
+    v-show="!memberItem?.login ? true : false"
+  >
+    <div class="rounded-full bg-slate-200 h-16 w-16"></div>
+    <div class="flex-1 space-y-6 py-1">
+      <div class="h-2 bg-slate-200 rounded"></div>
+      <div class="space-y-3">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="h-2 bg-slate-200 rounded col-span-1"></div>
         </div>
       </div>
+    </div>
   </div>
 </template>
