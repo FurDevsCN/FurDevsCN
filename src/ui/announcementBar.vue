15fe9ed3d310c4f6efbc5f6bac5037962c4dccb2
Steven Zhang <1581647952@qq.com>
2023-10-01 04:00:26 +0800

    feat(announcement): Add animation

--- src/ui/announcementBar.vue
+++ src/ui/announcementBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex items-center justify-center h-14 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full mt-5 max-sm:hidden"
+    class="flex shadow-lg animate-[notify_1s_ease-in-out_1] h-14 items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full mt-5 max-sm:hidden"
   >
     <slot></slot>
   </div>
