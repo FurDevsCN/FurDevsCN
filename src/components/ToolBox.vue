e4ecd854472860679e0e97e5e4a2b32547261f82
Steven Zhang <1581647952@qq.com>
2023-10-15 01:13:29 +0800

    feat(ToolBox): Dark Mode Toggle Button W.I.P.

---
+++ src/components/ToolBox.vue
@@ -0,0 +1,52 @@
+<template>
+    <transition name="slide" mode="out-in">
+        <div class="flex flex-col" v-show="showToolBox">
+            <ScrollToTop />
+            <DarkModeButton />
+        </div>
+    </transition>
+</template>
+<script lang="ts">
+import ScrollToTop from '../ui/ScrollToTop.vue';
+import DarkModeButton from '../ui/DarkModeButton.vue';
+export default {
+  components: {
+    ScrollToTop,
+    DarkModeButton,
+  },
+  data() {
+    return {
+      showToolBox: false,
+    };
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 100) {
+        this.showToolBox = true;
+      } else {
+        this.showToolBox = false;
+      }
+    },
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+};
+</script>
+<style>
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.5s;
+}
+
+.slide-leave-to /* .slide-leave-active 在 <2.1.8 中是 */ {
+  transform: translateX(300%);
+}
+
+.slide-enter-to {
+  transform: translateX(-0.25rem);
+}
+</style>
---
+++ src/ui/DarkModeButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <button
+        @click="toggleDarkMode"
+        class="fixed bottom-4 right-4 p-2 rounded-lg bg-gray-500 text-white shadow-lg hover:bg-gray-700"
+        key="scroll-to-top-button"
+      >
+        <div v-if="$data.darkMode">☀</div>
+        <div v-else>🌙</div>
+      </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { getLocalStorage, setLocalStorage } from '../utils/LocalStorage';
+export default {
+    data(){
+        return {
+            darkMode: getLocalStorage("darkMode")
+        }
+    },
+  methods: {
+    toggleDarkMode() {
+        this.darkMode === "true"? setLocalStorage("darkMode", "true"): setLocalStorage("darkMode", "false");
+    }
+  },
+  mounted() {
+    const darkMode = getLocalStorage("darkMode");
+    darkMode ? setLocalStorage("darkMode", darkMode): setLocalStorage("darkMode", "false");
+  }
+};
+</script>
+
+
