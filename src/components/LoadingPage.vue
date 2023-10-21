e4f13faa0a8fd899718cba59b04c03f59ff55bec
Steven Zhang <1581647952@qq.com>
2023-10-22 04:18:38 +0900

    fix(HomePage): Fix some style

--- src/components/LoadingPage.vue
+++ src/components/LoadingPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-screen items-center justify-center ">
+  <div class="flex h-screen items-center justify-center">
     <img src="/paw.svg" class="animate-bounce w-32 h-32 -z-100" />
   </div>
 </template>
